51575b00881f6d69cb8bfa9f79c0be6c6d41e23f
rayx <123456789+odin@users.noreply.github.com>
2025-10-08 15:47:33 +0800

    add score functionality

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const playerResult = document.getElementById("playerResult");
 const computerResult = document.getElementById("computerResult");
 const statusText = document.getElementById("statusText");
 
+const winScore = document.getElementById("winScore");
+const loseScore = document.getElementById("loseScore");
+const drawScore = document.getElementById("drawScore");
+
+let wins = 0;
+let loses = 0;
+let draw = 0;
+
 function generateComputerChoice() {
     const randomNumber = Math.random();
     if (randomNumber <= 0.33) {
@@ -24,6 +32,9 @@ function checkResults(player, computer){
         result = "It's a Tie!";
         statusText.innerHTML = result;
         document.querySelector(".status").style.backgroundColor = "yellow";
+        draw++;
+        drawScore.textContent = draw;
+        console.log(`Draw: ${draw}`);
     } else if (
         (player === "rock" && computer === "scissors") ||
         (player === "paper" && computer === "rock") ||
@@ -32,10 +43,16 @@ function checkResults(player, computer){
         result = "You Win! 🎉";
         statusText.innerHTML = result;
         document.querySelector(".status").style.backgroundColor = "green";
+        wins++
+        winScore.textContent = wins;
+        console.log(`Wins: ${wins}`);
     } else {
         result = "You Lose. 😢";
         statusText.innerHTML = result;
         document.querySelector(".status").style.backgroundColor = "red";
+        loses++;
+        loseScore.textContent = loses;
+        console.log(`Loses ${loses}`);
     }
 }
 
